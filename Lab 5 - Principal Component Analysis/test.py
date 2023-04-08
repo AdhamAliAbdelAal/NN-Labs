2a1881a0a99f981ad6e860edebaea263ad9a84d1
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2023-04-09 00:39:19 +0200

    TODO[3] not working

---
+++ Lab 5 - Principal Component Analysis/test.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+x=np.array([
+    [1,2,3],
+    [4,5,6],
+           ])
+
+print(np.cov(x,rowvar=False))
+
+x=np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+])
+w,v = np.linalg.eigh(x)
+print(w)
+print(v)
+print(np.flip(x,axis=0))
+
+x=np.array([
+    [1,2],
+])
+
+y=np.array([
+    [3,4],
+    [5,6],
+    [7,8],
+    [9,10],
+    [11,12],
+])
+
+print(x[np.newaxis,:,:].shape)
+x=x[np.newaxis,:,:]
+print(y[:,:,np.newaxis].shape)
+y=y[:,:,np.newaxis]
+print("***********")
+print((x@y))
