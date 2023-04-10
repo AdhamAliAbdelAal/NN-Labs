3878e9daf23e30743062b32dd62f1ba63f75e8a9
AdhamAliAbdelAal <adhamali20012015@gmail.com>
2023-04-10 17:17:44 +0200

    Add SVD

--- Lab 5 - Principal Component Analysis/test.py
+++ Lab 5 - Principal Component Analysis/test.py
@@ -1,22 +1,30 @@
 import numpy as np
 
 x=np.array([
-    [1,2],
-    [3,4],
-    [5,6],
+    [1,2,3,4,5,7],
+    [3,4,5,6,7,8],
+    [5,6,7,8,9,10],
+    [7,8,9,10,11,12],
+    [9,10,11,12,13,14],
+    [11,12,13,14,15,16],
         ])
 u=np.array([
     [10,20],
     [30,40],
 ])
+y=np.array([
+    1,3,4
+])
+print(x[:,y])
+
 def projectData(X, U, K):
     U= U[:K,:]
     Z = U[np.newaxis,:,:]@X[:,:,np.newaxis]
     return Z.reshape(-1,1)
 
 
-print(projectData(x,u,1))
-print(np.flip(x,axis=1))
+# print(projectData(x,u,1))
+# print(np.flip(x,axis=1))
 
 # print(np.cov(x,rowvar=False))
 
